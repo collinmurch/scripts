d9c35e0a5ac0944f5895dce2ffc561b4edc76602
Collin Murch <collin@murch.net>
2025-12-15 21:59:58 -0600

    update scripts

--- uuid.sh
+++ uuid.sh
@@ -2,14 +2,14 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title new uuid
+# @raycast.title uuid
 # @raycast.mode silent
 
 # Optional parameters:
 # @raycast.icon 🪪
 
 # Documentation:
-# @raycast.description copy a new uuid
+# @raycast.description copy a uuid
 # @raycast.author collin murch
 # @raycast.authorURL https://collinmurch.com
 
